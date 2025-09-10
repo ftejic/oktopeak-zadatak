13fc80208ed659a56659ea79234b3ebec4ed640b
Filip Tejic <ficatejic@gmail.com>
2025-09-10 15:06:35 +0200

    init vite react project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="text-center text-primary">Oktopeak - zadatak</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
